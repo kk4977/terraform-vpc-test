bff22d66c704d92689fbbc016c3840cc4e194076
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-05-17 12:59:05 +0530

    Code Updated  by sivakumar

--- vpc.tf
+++ vpc.tf
@@ -3,5 +3,7 @@ module "vpc" {
   common_tags = var.common_tags
   project_names = var.project_names
   public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  database_subnet_cidrs =var.database_subnet_cidrs
 
 }
