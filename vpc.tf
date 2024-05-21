3b5123275a7bc0a5bd6988e4d4260eccd26959ed
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-05-21 19:47:37 +0530

    Code Updated  by sivakumar

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
-  source = "../terraform-aws-vpc"
+  source = "git::https://github.com/daws-78s/terraform-aws-vpc.git?ref=main"
   common_tags = var.common_tags
-  project_names = var.project_names
+  project_name = var.project_names
   public_subnet_cidrs = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   database_subnet_cidrs =var.database_subnet_cidrs
